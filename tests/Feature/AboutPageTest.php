68a464e2b88e9e9e33816061c5ebb8c96ee8dd35
unknown <larasatigantari@gmail.com>
2026-09-08 20:19:09 +0700

    test: update test assertions for about page endpoint

--- tests/Feature/AboutPageTest.php
+++ tests/Feature/AboutPageTest.php
@@ -2,19 +2,15 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AboutPageTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function test_about_page_can_be_rendered(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/about');
 
         $response->assertStatus(200);
+        $response->assertSee('Tentang Aplikasi');
     }
 }
