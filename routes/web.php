07d9b4b1c0f1d7bd62ccbe7d4626723b6d96b8be
unknown <larasatigantari@gmail.com>
2026-09-08 19:55:13 +0700

    feat: tambah  about controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController;
+
+Route::get('/about', [AboutController::class, 'index'])->name('about.index');
 
 Route::get('/', function () {
     return view('welcome');
